c68ee75b588f0812ef44a09913940153c929d7d6
brent <brent.e.meyer@gmail.com>
2018-03-26 01:29:35 -0400

    Adds basic grid

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 (function() {
 
   Reveal.initialize({
+    width: "100%",
+    height: "100%",
+    margin: 0,
+    minScale: 1,
+    maxScale: 1,
     dependencies: [
       { src: './js/reveal.js-3.6.0/plugin/markdown/marked.js' },
       { src: './js/reveal.js-3.6.0/plugin/markdown/markdown.js' },
